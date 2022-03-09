014eb4d96e823b7001a5f180c05cc89ef441d2c4
chauhanhritik <chauhanghritik@gmail.com>
2022-03-09 19:34:14 +0530

    commit after databse connection

--- env_creation/server.js
+++ env_creation/server.js
@@ -1,14 +1,19 @@
+if(process.env.NODE_ENV !== 'production'){//we want to load env file only if we are in a development
+    require('dotenv').config();  //loads all variables from dotenv
+}
+
+
 // npm run devstart --> used to run the server using devstart script
 
 
 const express=require("express")
 const app=express() 
-
 // Routes are integrated with views using layouts
 // we define boiler plate for every page using layouts
 const  expressLayouts = require('express-ejs-layouts')
 const indexRouter=require('./routes/index') //using the route we have built
 // INDEXROUTER is a reference to route within index.js
+const mongoose = require('mongoose')
 
 
 // Setting view engines-
@@ -29,3 +34,13 @@ app.use('/',indexRouter)
 app.listen(process.env.PORT||3000)  // when run without routes error will be shown in browser :: Cannot GET /
 // routes can be addded within server.js only or can be defined externally too based on complexity of the program
 // ROUTES ARE CONTROLLERS
+
+
+// setting a connection for database using mongoose
+mongoose.connect(process.env.DATABASE_URL,{
+    useNewUrlParser: true
+})
+const db = mongoose.connection
+db.on('error', error=>{console.error(error)}) //if the connection is not made and an error is returned, then print the error in console
+db.once('open', error=>{console.log('Connected to mongoose')}) //Runs only once when the connection is made/opened
+
